971072237db617248b79b8e3026172981dd85e20
Geraldmo122 <81269879+Geraldmo122@users.noreply.github.com>
2024-03-03 00:05:06 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,22 @@
+var typed = new Typed('.element', {
+    strings: ['','Ingeniero informático','' , 'Diseñador','', 'Freelance'],
+    typedSpeed: 200,
+    backDelay: 2000,
+    backspeed: 500,
+    loop:true
+  });
+
+var typed = new Typed('.name', {
+    strings: ['', 'Gerald Morales'],
+    typeSpeed : 40 ,
+    backDelay : 700 ,
+    backSpeed: 100,
+    loop: true,    
+});
+var typed = new Typed('.element2', {
+    strings: ['', 'Ingeniero informático','' ,'Diseñador','', 'Freelance'],
+    typedSpeed: 200,
+    backDelay: 2000,
+    backspeed: 500,
+    loop:true
+  });
